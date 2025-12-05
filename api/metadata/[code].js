f38059fd4aa2cccfd990a49431a32c552019f9c3
ahmadyani1st <151477384+ahmadyani1st@users.noreply.github.com>
2025-12-05 11:12:30 +0700

    Update [code].js

--- api/metadata/[code].js
+++ api/metadata/[code].js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
       data: {
         targetUrl: data?.linkproduk,
         metadata: {
-          title: data?.name,
+          title: data?.title,
           description: data?.description,
           image: data?.image
         }
